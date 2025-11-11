5068dd469dded46ab09e62f2390d4371c167874a
g3ntil <ntwarigentil70@gmail.com>
2025-11-11 15:41:04 +0200

    vercel screen fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
+  base: "/", 
   root: './src',
   publicDir: '../public',
   plugins: [react()],
